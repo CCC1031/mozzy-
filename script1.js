d6b2cd369e770464a16187390ad4e99480410476
Colin-techdev <clarencecolin@gmail.com>
2022-08-05 19:04:16 -0400

    Edits

--- script1.js
+++ script1.js
@@ -1,57 +1,57 @@
 
-    const songImage = document.getElementById('song-image');
-    const songTitle = document.getElementById('song-title');
-    const songArtist = document.getElementById('song-artist');
+    // const songImage = document.getElementById('song-image');
+    // const songTitle = document.getElementById('song-title');
+    // const songArtist = document.getElementById('song-artist');
     
-    const playBtn = document.getElementById('play-btn');
-    const prevBtn = document.getElementById('prev-btn');
-    const nextBtn = document.getElementById('next-btn');
+    // const playBtn = document.getElementById('play-btn');
+    // const prevBtn = document.getElementById('prev-btn');
+    // const nextBtn = document.getElementById('next-btn');
 
-    const audioPlayer = document.getElementById('music-player');
+    // const audioPlayer = document.getElementById('music-player');
 
-    let currentSongIndex;
-    let nextSong;
+    // let currentSongIndex;
+    // let nextSong;
 
-    let songs = [
-        {
-            title: 'Rooftops',
-            artist: 'Wonderland',
-            song_path: 'audio/wonder.mp3',
-            img_path: 'img/j.jpeg',
-        },
-        {
-            title: 'Season Finale',
-            artist: 'Beò',
-            song_path: 'audio/wonder.mp3',
-            img_path: 'img/Wonder.jpeg',
-        }
-    ]
+    // let songs = [
+    //     {
+    //         title: 'Rooftops',
+    //         artist: 'Wonderland',
+    //         song_path: 'audio/wonder.mp3',
+    //         img_path: 'img/j.jpeg',
+    //     },
+    //     {
+    //         title: 'Season Finale',
+    //         artist: 'Beò',
+    //         song_path: 'audio/wonder.mp3',
+    //         img_path: 'img/Wonder.jpeg',
+    //     }
+    // ]
 
-    playBtn.addEventListener('click', TogglePlaySong);
+    // playBtn.addEventListener('click', TogglePlaySong);
 
-    InitPlayer();
+    // InitPlayer();
 
-    function InitPlayer() {
-        currentSongIndex = 0;
-        UpdatePlayer();
-    }
+    // function InitPlayer() {
+    //     currentSongIndex = 0;
+    //     UpdatePlayer();
+    // }
 
-    function UpdatePlayer() {
-        let song = songs[currentSongIndex];
+    // function UpdatePlayer() {
+    //     let song = songs[currentSongIndex];
 
-        songImage.style = "backgroud-image: url('" + song.img_path + "')";
-        songTitle.innerText = song.title; 
-        songArtist.innerText = song.artist; 
-    }
+    //     songImage.style = "backgroud-image: url('" + song.img_path + "')";
+    //     songTitle.innerText = song.title; 
+    //     songArtist.innerText = song.artist; 
+    // }
 
-    function TogglePlaySong () {
-        if (audioPlayer.paused) {
-            audioPlayer.play();
-        }
-        else {
-            audioPlayer.pause();
-        }
-    }
+    // function TogglePlaySong () {
+    //     if (audioPlayer.paused) {
+    //         audioPlayer.play();
+    //     }
+    //     else {
+    //         audioPlayer.pause();
+    //     }
+    // }
 
 
 
